985a7351c1a0317e06066b0d80a8bbe8208054e1
xiyuy <yang.xiyu@northeastern.edu>
2024-09-21 12:02:15 -0400

    Update datasets.py os.path.join

--- src/datasets.py
+++ src/datasets.py
@@ -8,8 +8,8 @@ from torch.utils.data import Dataset
 class JoslinData(Dataset):
     def __init__(self, data_dir, annotations_file, img_dir, transform=None,
                  target_transform=None):
-        self.img_labels = pd.read_csv(os.join(data_dir, annotations_file))
-        self.img_dir = os.join(data_dir, img_dir)
+        self.img_labels = pd.read_csv(os.path.join(data_dir, annotations_file))
+        self.img_dir = os.path.join(data_dir, img_dir)
         self.transform = transform
         self.target_transform = target_transform
 
