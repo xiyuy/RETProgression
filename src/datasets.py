a50a8d773f8fe4c9f4fc2ebbb272a2455dd4b9bd
labdhigandhi <labdhi.gamdhi@gmail.com>
2024-09-26 10:29:17 -0400

    debugging

--- src/datasets.py
+++ src/datasets.py
@@ -22,13 +22,13 @@ class JoslinData(Dataset):
         return len(self.img_labels)
 
     def __getitem__(self, idx):
-        # img_file_name = str(self.img_labels.iloc[idx, 0]) + '.jpeg' 
-        # img_path = os.path.join(self.img_dir, img_file_name)
-        # image = read_image(img_path)
-        # label = self.img_labels.iloc[idx, 1]
-        img_path = os.path.join(self.img_dir, self.img_labels.iloc[idx, 0])
+        img_file_name = str(self.img_labels.iloc[idx, 1]) + '.jpeg' 
+        img_path = os.path.join(self.img_dir, img_file_name)
         image = read_image(img_path)
-        label = self.img_labels.iloc[idx, 1]
+        # label = self.img_labels.iloc[idx, 1]
+        # img_path = os.path.join(self.img_dir, self.img_labels.iloc[idx, 0])
+        # image = read_image(img_path)
+        label = self.img_labels.iloc[idx, 3]
         # image = image.float() / 255.0
         if self.transform:
             image = self.transform(image)
